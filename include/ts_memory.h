4480bccb7d26002a72882eb221ad6789d91d25d8
Argishti Ayvazyan <argishty.ayvazyan@outlook.com>
2021-07-14 22:34:50 +0400

    Initial commit. Implemented make system.

---
+++ include/ts_memory.h
@@ -0,0 +1,18 @@
+#ifndef THREADSAFESMARTPOINTERS_TS_MEMORY_H
+#define THREADSAFESMARTPOINTERS_TS_MEMORY_H
+
+/**
+ * @file        ts_memory.h
+ * @author      Argishti Ayvazyan (ayvazyan.argishti@gmail.com)
+ * @brief       Declaration and implementations of thread-safe smart pointers.
+ * @date        7/14/2021
+ * @copyright   Copyright (c) 2021
+ */
+
+
+namespace ts
+{
+
+} // namespace ts
+
+#endif //THREADSAFESMARTPOINTERS_TS_MEMORY_H
---
+++ tests/main.cc
@@ -0,0 +1,18 @@
+/**
+ * @file        main.cc
+ * @author      Argishti Ayvazyan (ayvazyan.argishti@gmail.com)
+ * @brief       Unit tests implementations.
+ * @date        7/14/2021
+ * @copyright   Copyright (c) 2021
+ */
+
+#include <iostream>
+
+#include <ts_memory.h>
+
+
+int main()
+{
+    std::cout << "Hello multithreading." << std::endl;
+    return 0;
+}
